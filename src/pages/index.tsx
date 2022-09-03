65065f19b6817510e70655aef90a84109fc199aa
End313234 <end313234@protonmail.com>
2022-09-03 11:49:26 -0300

    Fix visual bug on mobile devices

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,20 +37,25 @@ const Home: NextPage = () => {
 						className="hidden md:flex"
 					/>
 				</div>
-				<div id="product">
+				<div id="product" className="selection:bg-blue-600	">
 					<img
 						src="/top-wave.svg"
 						alt="Wave"
 						className="relative top-1 md:top-0"
 					/>
-					<div className="bg-brown flex flex-col items-center justify-center text-white gap-5 h-[30vh] md:h-[50vh]">
+					<div className="bg-brown flex flex-col items-center justify-center text-white gap-5 py-10">
 						<div className="flex flex-col items-center justify-center gap-3">
 							<h1 className="text-center">Conheça a TerraNossa!</h1>
-							<p className="max-w-[50vw] text-center">
+							<p className="min-w-1/2 w-[80vw] text-center md:w-[50vw]">
 								O produto que vai garantir conforto, qualidade e
-								sustentabilidade à alimentação do seu dia-a-dia.
+								sustentabilidade à alimentação do seu dia-a-dia. Lorem ipsum
+								dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua.
 							</p>
 						</div>
+						<button className="bg-transparent border border-white px-5 py-4 rounded-md transition-all hover:bg-white hover:text-brown">
+							Saiba o tamanho exato para você
+						</button>
 					</div>
 				</div>
 			</main>
